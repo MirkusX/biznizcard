de8d6972c5b9b00f7f5a8016e81b4c711b4459fa
MirkusX <marcusk.kodehode@gmail.com>
2023-01-05 14:16:12 +0100

    added background colors

---
+++ src/Components/StyledComponents.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  background-color: #1a1b21;
+  width: 25%;
+  margin: 0 auto;
+`;
+
+export const StyledSection = styled.section`
+  background-color: #23252c;
+  min-height: 100vh;
+`;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,7 +1,23 @@
+import { StyledDiv, StyledSection } from "../Components/StyledComponents";
+
 export const Frontpage = () => {
   return (
-    <section>
-      <h1>BiznizCard</h1>
-    </section>
+    <StyledSection>
+      <StyledDiv>
+        <h1>Example name</h1>
+        <p>Examplejob</p>
+        <p>website</p>
+        <button>
+          <a>Email</a>
+        </button>
+        <h2>About</h2>
+        <p>example</p>
+        <h2>interests</h2>
+        <p>example</p>
+        <div>
+          <p>logos</p>
+        </div>
+      </StyledDiv>
+    </StyledSection>
   );
 };
