3a72ed036a4a02d1391bd7849ad33ff9cda3c675
MirkusX <marcusk.kodehode@gmail.com>
2023-01-06 10:29:34 +0100

    further styling, added icon for email

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import biznizcard from "../images/biznizcard.avif";
+import { HiOutlineMail } from "react-icons/hi";
 
 export const StyledDiv = styled.div`
   background-color: #1a1b21;
   width: 25%;
   border-radius: 10px;
+  color: white;
 `;
 
 export const StyledSection = styled.section`
@@ -16,13 +18,34 @@ export const StyledSection = styled.section`
   padding: 3em;
 `;
 
-export const StyledImg = styled.img`
-  width: 100%;
-`;
-
 export const StyledImgDiv = styled.div`
   background-image: url(${biznizcard});
   height: 20em;
   background-position: 60% 20%;
   border-radius: 10px 10px 0 0;
+`;
+
+export const StyledButton = styled.button`
+  width: 80%;
+  padding: 0.5em;
+  font-size: 0.875rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  cursor: pointer;
+  gap: 0.5em;
+`;
+
+export const MailIcon = styled(HiOutlineMail)``;
+
+export const PDiv = styled.div`
+  text-align: left;
+  width: 80%;
+  margin: 0 auto;
+`;
+
+export const StyledP = styled.p`
+  color: #dcdcdc;
+  font-size: 0.64rem;
 `;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,7 +1,12 @@
 import {
+  MailIcon,
+  PDiv,
+  StyledButton,
   StyledDiv,
   StyledImg,
   StyledImgDiv,
+  StyledP,
+  StyledPDiv,
   StyledSection,
 } from "../Components/StyledComponents";
 import biznizcard from "../images/biznizcard.avif";
@@ -12,16 +17,31 @@ export const Frontpage = () => {
     <StyledSection>
       <StyledDiv>
         <StyledImgDiv />
-        <h1>Example name</h1>
-        <p>Examplejob</p>
-        <p>website</p>
-        <button>
+        <h1>Laura Smith</h1>
+        <p>Front End Developer</p>
+        <p>
+          <a href="https://mirkusx.github.io/My_portfolio/">
+            MirkusX.portfolio
+          </a>
+        </p>
+        <StyledButton>
+          <MailIcon />
           <a>Email</a>
-        </button>
-        <h2>About</h2>
-        <p>example</p>
-        <h2>interests</h2>
-        <p>example</p>
+        </StyledButton>
+        <PDiv>
+          <h2>About</h2>
+          <StyledP>
+            I am not Laura Smith this is a Scrimba Solo Project, if you want to
+            see my projects and portfolio click the link above. I focus mainly
+            on React.
+          </StyledP>
+          <h2>Interests</h2>
+          <StyledP>
+            The icons below goes to my portfolio, the image is from unsplash I
+            was lucky to find the same image used on the template of the solo
+            Scrimba project.
+          </StyledP>
+        </PDiv>
         <div>
           <p>logos</p>
         </div>
