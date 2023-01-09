255d098f6ced557a5eb59e9933be654f8f031c55
MirkusX <marcusk.kodehode@gmail.com>
2023-01-09 10:34:41 +0100

    added icons, updated stylings

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import biznizcard from "../images/biznizcard.avif";
 import { HiOutlineMail } from "react-icons/hi";
+import {
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithubSquare,
+} from "react-icons/fa";
 
 export const StyledDiv = styled.div`
   background-color: #1a1b21;
@@ -25,7 +32,7 @@ export const StyledImgDiv = styled.div`
   border-radius: 10px 10px 0 0;
 `;
 
-export const StyledButton = styled.button`
+export const StyledButton = styled.a`
   width: 80%;
   padding: 0.5em;
   font-size: 0.875rem;
@@ -35,6 +42,10 @@ export const StyledButton = styled.button`
   margin: 0 auto;
   cursor: pointer;
   gap: 0.5em;
+  background: white;
+  color: black;
+  border-radius: 6px;
+  text-decoration: none;
 `;
 
 export const MailIcon = styled(HiOutlineMail)``;
@@ -48,4 +59,78 @@ export const PDiv = styled.div`
 export const StyledP = styled.p`
   color: #dcdcdc;
   font-size: 0.64rem;
+  margin-top: 0;
+  ${(props) => {
+    if (props.job)
+      return `
+    font-size: 0.8rem;
+    margin-bottom: 0.550625em;
+    color: #F3BF99;
+    ;`;
+  }}
+  ${(props) => {
+    if (props.link)
+      return `
+    font-size: 0.64rem;
+    margin-bottom: 1.5em;
+    `;
+  }}
+`;
+
+export const StyledLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  &:hover {
+    color: blue;
+  }
+`;
+
+export const StyledH1 = styled.h1`
+  margin-bottom: 0;
+`;
+
+export const StyledH2 = styled.h2`
+  margin-bottom: 0;
+`;
+
+export const Footer = styled.div`
+  background-color: #161619;
+  border-radius: 0 0 10px 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1em 3em;
+`;
+
+export const Space = styled.div`
+  margin: 1.5em;
+`;
+
+export const TwitterIcon = styled(FaTwitterSquare)`
+  color: #918e9b;
+  width: 25px;
+  height: 25px;
+`;
+
+export const FacebookIcon = styled(FaFacebookSquare)`
+  color: #918e9b;
+  width: 25px;
+  height: 25px;
+`;
+
+export const InstagramIcon = styled(FaInstagramSquare)`
+  color: #918e9b;
+  width: 25px;
+  height: 25px;
+`;
+
+export const LinkedInIcon = styled(FaLinkedin)`
+  color: #918e9b;
+  width: 25px;
+  height: 25px;
+`;
+
+export const GithubIcon = styled(FaGithubSquare)`
+  color: #918e9b;
+  width: 25px;
+  height: 25px;
 `;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,50 +1,75 @@
 import {
+  FacebookIcon,
+  Footer,
+  GithubIcon,
+  InstagramIcon,
+  LinkedInIcon,
   MailIcon,
   PDiv,
+  Space,
   StyledButton,
   StyledDiv,
-  StyledImg,
+  StyledH1,
+  StyledH2,
   StyledImgDiv,
+  StyledLink,
   StyledP,
-  StyledPDiv,
   StyledSection,
+  TwitterIcon,
 } from "../Components/StyledComponents";
-import biznizcard from "../images/biznizcard.avif";
-/* <StyledImg src={biznizcard} /> */
 
 export const Frontpage = () => {
   return (
     <StyledSection>
       <StyledDiv>
         <StyledImgDiv />
-        <h1>Laura Smith</h1>
-        <p>Front End Developer</p>
-        <p>
-          <a href="https://mirkusx.github.io/My_portfolio/">
+        <StyledH1>Laura Smith</StyledH1>
+        <StyledP job>Front End Developer</StyledP>
+        <StyledP link>
+          <StyledLink
+            href="https://mirkusx.github.io/My_portfolio/"
+            target="_blank"
+            rel="noreferrer"
+          >
             MirkusX.portfolio
-          </a>
-        </p>
-        <StyledButton>
+          </StyledLink>
+        </StyledP>
+        <StyledButton href="mailto:marcusk.kodehode@gmail.com">
           <MailIcon />
-          <a>Email</a>
+          Email
         </StyledButton>
         <PDiv>
-          <h2>About</h2>
+          <StyledH2>About</StyledH2>
           <StyledP>
             I am not Laura Smith this is a Scrimba Solo Project, if you want to
             see my projects and portfolio click the link above. I focus mainly
             on React.
           </StyledP>
-          <h2>Interests</h2>
+          <StyledH2>Interests</StyledH2>
           <StyledP>
-            The icons below goes to my portfolio, the image is from unsplash I
-            was lucky to find the same image used on the template of the solo
-            Scrimba project.
+            The icons below goes to my GitHub profile, the image is from
+            unsplash. I was lucky to find the same image used on the template of
+            the solo Scrimba project.
           </StyledP>
         </PDiv>
-        <div>
-          <p>logos</p>
-        </div>
+        <Space />
+        <Footer>
+          <a href="https://github.com/MirkusX" target="_blank" rel="noreferrer">
+            <TwitterIcon />
+          </a>
+          <a href="https://github.com/MirkusX" target="_blank" rel="noreferrer">
+            <FacebookIcon />
+          </a>
+          <a href="https://github.com/MirkusX" target="_blank" rel="noreferrer">
+            <InstagramIcon />
+          </a>
+          <a href="https://github.com/MirkusX" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+          <a href="https://github.com/MirkusX" target="_blank" rel="noreferrer">
+            <GithubIcon />
+          </a>
+        </Footer>
       </StyledDiv>
     </StyledSection>
   );
